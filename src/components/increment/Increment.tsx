542c23a762cd2e2de7284b1bb01b5e82f91ccbec
Alexandre <avanryssel@gmail.com>
2022-10-20 13:52:26 +0200

    init increment app

--- src/components/increment/Increment.tsx
+++ src/components/increment/Increment.tsx
@@ -1,4 +1,27 @@
+import React from 'react';
+import { TezosToolkit } from '@taquito/taquito';
+import { useEffect } from 'react';
+
+const contractAddress: string = "KT1K1o7Uep1ChtgcPKRSQBxJ746jfW5T46NX";
+const Tezos: TezosToolkit = new TezosToolkit('https://rpc.ghostnet.teztnets.xyz/');
+
 function Increment() {
+
+    useEffect(() => {
+        (async () => {
+
+            Tezos.contract
+            .at(contractAddress)
+            .then((c) => {
+                let methods = c.parameterSchema.ExtractSignatures();
+                console.log(JSON.stringify(methods, null, 2));
+            })
+            .catch((error) => console.log(`Error: ${error}`));
+                
+        })();
+    }, []);
+
+
     return (
         <>
         Increment
